2adc2b7e802ef44ea12e426444402032cea8f26c
Renato de Oliveira Boranga <m318311@alunos.ipb.pt>
2024-08-09 22:16:06 -0300

    Initial commit

---
+++ p2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define LENGTH 8     
+
+int main() {
+    
+    int vetor[LENGTH];     
+        
+    for(int i=0; i < LENGTH; i++){
+        printf("Entre com o numero %i: ",i+1);
+        scanf("%i",&vetor[i]);
+    }    
+        
+    printf("Valores repetidos: ");    
+    // Procura os repetidos   
+    for(int j = 0; j < LENGTH ; j++) {    
+        for(int k = j + 1; k < LENGTH; k++) {    
+            if(vetor[j] == vetor[k])    
+                printf("%d, ", vetor[k-1]);    
+        }    
+    }   
+    return 0;
+}
